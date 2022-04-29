66b4bb2326839221346ce3de1807b06b62faaa6a
emppu1337 <emiln.niemi@gmail.com>
2022-04-29 15:51:27 +0200

    optimized imports

--- src/main/java/LoginTest.java
+++ src/main/java/LoginTest.java
@@ -3,14 +3,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
-import java.util.function.Function;
 
 public class LoginTest {
 
